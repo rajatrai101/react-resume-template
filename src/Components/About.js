ea4865f2d7adfdb8ffa6a6948b0b8e3dfcd12498
Rajat Rai <rajatrai1206@gmail.com>
2020-03-01 21:30:27 -0500

    Initial commit

--- src/Components/About.js
+++ src/Components/About.js
@@ -20,7 +20,7 @@ class About extends Component {
       <section id="about">
       <div className="row">
          <div className="three columns">
-            <img className="profile-pic"  src={profilepic} alt="Tim Baker Profile Pic" />
+            <img className="profile-pic"  src={profilepic} alt="Rajat Rai Profile Pic" />
          </div>
          <div className="nine columns main-col">
             <h2>About Me</h2>
@@ -31,8 +31,10 @@ class About extends Component {
                   <h2>Contact Details</h2>
                   <p className="address">
 						   <span>{name}</span><br />
-						   <span>{street}<br />
-						         {city} {state}, {zip}
+                         <span>
+                            {/* {street}<br /> */}
+                            {city} <br />
+                            {state}, {zip}
                    </span><br />
 						   <span>{phone}</span><br />
                      <span>{email}</span>
